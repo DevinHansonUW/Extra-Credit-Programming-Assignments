12ea11ae386caad0d831ed27a238078c154d7615
Devin  Hanson <devingh2@uw.edu>
2023-06-27 21:13:00 -0700

    fixing randomization

--- automata.js
+++ automata.js
@@ -36,13 +36,13 @@ class Automata {
 
         for (let col = 0; col < this.width; col++) {
             for (let row = 0; row < this.height; row++) {
-                chanceAlive = Math.floor(Math.random() * this.randomChance);
-
                 if (chanceAlive >= this.randomChance/2) {
                     this.entities[col][row] = 1;
                 } else {
                     this.entities[col][row] = 0;
                 }
+
+                chanceAlive = Math.floor(Math.random() * this.randomChance);
             }
         }
     }
